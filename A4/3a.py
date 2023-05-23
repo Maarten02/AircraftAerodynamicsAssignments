fae0fd7d890be9c114e34cc64a2ece00d19c3d6b
Maarten02 <m.e.krikke@student.tudelft.nl>
2023-05-23 11:42:19 +0200

    add 3a progress

--- A4/3a.py
+++ A4/3a.py
@@ -11,12 +11,6 @@ import csv
 # Sref Cref Bref
 # 105.0 3.75 28.0
 Snw = 105           # [m^2]
-
-# --- WINGLET ---
-# Sref Cref Bref
-# 108.136 3.862 28.0
-Swwl = 108.136      # [m^2]
-
 rho = 0.73612       # [kg/m^3]
 v = 0.5 * 320.53    # [m/s]
 
@@ -24,38 +18,28 @@ v = 0.5 * 320.53    # [m/s]
 #================ END NOTES ===================
 
 
-def fixed_cant(angle=0, tip_length=0.08, Nchordwise=12, Nspanwise=12, clear_dirs=False):
-    X_ow_le = 3.5
+def vary_dihedral(dih_angle, Nchordwise=12, Nspanwise=12, clear_dirs=False):
+    x = 3.5
     Y_ow_le = 14
-    Z_ow_le = 0
-    wing_end_c = 2
-    taper = 0.4
-    Cwr = 1
-    Lambda = 15/180 * math.pi
-    lw = tip_length * 2 * Y_ow_le
-
-    span = 2 * (Y_ow_le + lw)
-    Sref = (5.5 + 2) * 0.5 * span + 2 * (1 + taper) * lw
+    span = 2 * (Y_ow_le)
+    Sref = (5.5 + 2) * 0.5 * span
     Cref = Sref / span
 
     # span = 28
     # Sref = span * 0.5 * (5.5 + 2)
     # Cref = Sref / span
 
+    dih_angle = (90 - dih_angle)/180 * math.pi
+
     # --- Determine Xle, Yle, Zle for the wingtip for N cant angles ---
-    dX = math.tan(Lambda) * lw
-    if angle == 0:
-        dY = 0
-        dZ = lw
+    if dih_angle == 0:
+        z = 0
+        y = Y_ow_le
     else:
-        dY = lw / math.sqrt(1 + 1/math.tan(angle) ** 2)
-        dZ = dY / math.tan(angle)
+        y = Y_ow_le / math.sqrt(1 + 1/math.tan(dih_angle) ** 2)
+        z = y / math.tan(dih_angle)
 
-    x = X_ow_le + wing_end_c - Cwr*wing_end_c + dX
-    y = Y_ow_le + dY
-    z = Z_ow_le + dZ
-
-    winglet = f'{x:.3f} {y:.3f} {z:.3f}'
+    tip = f'{x:.3f} {y:.3f} {z:.3f}'
 
 
     # --- delete all input and output files ---
@@ -69,24 +53,24 @@ def fixed_cant(angle=0, tip_length=0.08, Nchordwise=12, Nspanwise=12, clear_dirs
 
     # -------- write the .avl files --------
     # Open the geom_template.avl file
-    with open('geom_template.avl', 'r') as f:
+    with open('geom_no_winglet.avl', 'r') as f:
         contents = f.readlines()
 
     # Modify line 43
     contents[6] = f'{Sref:.3f} {Cref:.3f} {span:.3f}\n'
     contents[15] = f'{Nchordwise} 1.0 {Nspanwise} 1.0\n'
-    contents[42] = winglet + contents[42][18:]
+    contents[34] = tip + contents[34][18:]
 
     # Write the modified contents to a new file
     tlf = round(tip_length*100)
-    filename = f'cant_list_in/cant_{angle}_lw{tlf}.avl'
+    filename = f'dih_list_in/dih_{dih_angle}_lw{tlf}.avl'
     with open(filename, 'w') as f:
         f.writelines(contents)
 
     # --- Run AVL.exe with the newly generated .avl files ---
     # Start subprocess and pass input commands as a string
     avl_process = subprocess.Popen('avl.exe', stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-    input_string = f'load ./cant_list_in/cant_{angle}_lw{tlf}.avl\n' \
+    input_string = f'load ./dih_list_in/cant_{angle}_lw{tlf}.avl\n' \
                    'case ./case.run\n' \
                    'oper\n' \
                    'x\n'
